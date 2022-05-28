116aeab5fb7715040e91faeb2a9231424ce4d42c
Fernando Rebelo <94297628+fernandorebelo@users.noreply.github.com>
2022-05-28 17:24:23 -0300

    Update Principal.java

--- DesafioAula10Parte4/src/Principal.java
+++ DesafioAula10Parte4/src/Principal.java
@@ -5,27 +5,38 @@ import classes.Usuario;
 public class Principal {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "CADASTRO DE USU¡RIO");
+		JOptionPane.showMessageDialog(null, "CADASTRO DE USU√ÅRIO");
 
+		String nome = JOptionPane.showInputDialog("Cadastro - Nome: ").toUpperCase();
+		String email;
+		do {
+			email = JOptionPane.showInputDialog(nome + "\nCadastro - E-mail: ").toUpperCase();
+			Usuario.validarEmail(email);
+			if(Usuario.validarEmail(email) == false) {
+				JOptionPane.showMessageDialog(null, "E-mail inv√°lido! Digite novamente!");
+			}
+		} while (Usuario.validarEmail(email) == false);
+		String login = JOptionPane.showInputDialog(nome + "\n" + email + "\nCadastro - Login: ");
+		String senha = JOptionPane.showInputDialog(nome + "\n" + email + "\nCadastro - Senha: ");
 		Usuario usuario = new Usuario();
-		usuario.cadastroUsuario();
+		usuario.cadastroUsuario(nome, email, login, senha);
 		
-		JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso! \nInforma seu login e senha para logar: ");
+		JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso! \nInforme seu login e senha para logar: ");
 		
 		boolean condicao = false;
 		do {
-			String loginAut = JOptionPane.showInputDialog("AutenticaÁ„o - Login: ");
-			String senhaAut = JOptionPane.showInputDialog("AutenticaÁ„o - Senha: ");
+			String loginAut = JOptionPane.showInputDialog("Autentica√ß√£o - Login: ");
+			String senhaAut = JOptionPane.showInputDialog("Autentica√ß√£o - Senha: ");
 			
 			Usuario usuarioAut = new Usuario();
 			usuarioAut.login = loginAut;
 			usuarioAut.senha = senhaAut;
 			
 			if(usuario.login.equals(usuarioAut.login) && usuario.senha.equals(usuarioAut.senha)) {
-				JOptionPane.showMessageDialog(null, "Usu·rio logado com sucesso!");
+				JOptionPane.showMessageDialog(null, "Usu√°rio logado com sucesso!");
 				condicao = false;
 			}else{
-				JOptionPane.showMessageDialog(null, "Login ou Senha inv·lidos! Tente novamente!");
+				JOptionPane.showMessageDialog(null, "Login ou Senha inv√°lidos! Tente novamente!");
 				condicao = true;
 			}
 		} while (condicao);
